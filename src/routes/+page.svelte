360deb3bcfa5e6d8078c119e46c1429dc597cb91
Mateus Tymoniuk <tymoniuk.m@gmail.com>
2024-06-01 00:11:15 +0200

    Add form page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import qrcode from "qrcode-generator";
+
+    let name = "";
+    let email = "";
+    let twitter = "";
+    let github = "";
+
+    function cancel() {
+        name = "";
+        email = "";
+        twitter = "";
+        github = "";
+    }
+
+    function generateBadge() {
+        let qr = qrcode(4, 'L');
+        qr.addData("Hi");
+        qr.make();
+        document.getElementById("placeholder").innerHTML = qr.createImgTag();
+    }
+</script>
+<h1>Badge QRcode generator</h1>
+<form>
+    <label for="name">Name: </label>
+    <input bind:value={name} name="name" type="text" required />
+    <label for="email">Email: </label>
+    <input bind:value={email} name="email" type="text" required />
+    <label for="twitter">Twitter: </label>
+    <input bind:value={twitter} name="twitter" type="text" />
+    <label for="github">Github: </label>
+    <input bind:value={github} name="github" type="text" />
+    <button on:click="{cancel}">Cancel</button>
+    <button on:click="{generateBadge}">Create</button>
+</form>
+<h2>
+    debug
+</h2>
+<p>{name}</p>
+<p>{email}</p>
+<p>{twitter}</p>
+<p>{github}</p>
+<div id="placeholder"></div>
+
