fa0f65586efe237c4d638f1b6d4745f76ff38c83
Mateus Tymoniuk <tymoniuk.m@gmail.com>
2024-06-16 22:02:14 +0200

    Add badge page
    - This page should be the one that the visitor sees when reading the
      qrcode from someone's badge

---
+++ src/routes/badge/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { page } from '$app/stores';
+
+    const hasParams = $page.url.searchParams.has("name");
+    const name = $page.url.searchParams.get("name");
+
+    let email;
+    if ($page.url.searchParams.has("email")) {
+        email = $page.url.searchParams.get("email");
+    }
+
+    let github;
+    if ($page.url.searchParams.has("github")) {
+        github = $page.url.searchParams.get("github");
+    }
+
+    let twitter;
+    if ($page.url.searchParams.has("twitter")) {
+        twitter = $page.url.searchParams.get("twitter");
+    }
+</script>
+
+<h2>Atendee name:</h2>
+<p>{name}</p>
+<h2>Email:</h2>
+<p>{email}</p>
+<h2>Github profile:</h2>
+<a href="https://github.com/{github}">{github}</a>
+<h2>Twitter profile:</h2>
+<a href="https://x.com/{twitter}">{twitter}</a>
