a7fc7a686b5742b0964b1c0873f71994f0716151
Mateus Tymoniuk <tymoniuk.m@gmail.com>
2024-06-16 21:58:37 +0200

    Add Badge component

---
+++ src/lib/Badge.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let name;
+    export let qrcode;
+</script>
+<div>
+    <p>Here's the badge!</p>
+    <img src="avatar_placeholder_large.png" />
+    <p>{name}</p>
+    <img src={qrcode} />
+</div>
+
